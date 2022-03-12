a99cc0f24cdaf9e98f5085c50e7c7555d10d9423
Carolina M <carolinamargiott2i@gmail.com>
2022-03-12 15:30:30 -0300

    terminado navegação basica

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint, current_app
 
 from controllers.news_controller import home as news_home
 from controllers.news_controller import newsPost as news_post
+from controllers.news_controller import homeSubject as news_home_subject
 
 template_dir = os.path.abspath('sitedenoticiasuser/templates/')
 
@@ -11,6 +12,10 @@ news_bp = Blueprint("news",__name__,template_folder=template_dir)
 @news_bp.route("/")
 def home():
     return news_home()
+
+@news_bp.route("/<string:subject>")
+def homeSubject(subject):
+    return news_home_subject(subject)
 
 @news_bp.route("/newsPost/<int:postId>")
 def newsPost(postId):
--- controllers/news_controller.py
+++ controllers/news_controller.py
@@ -2,7 +2,16 @@ from flask import request, jsonify, render_template
 
 #import dos models
 
-noticias = [{'titulo':"porquinhos da india",'assunto':"pets",'conteudo':"cuicuicuicuicui"}]
+noticias = [
+{'id':0,'titulo':"porquinhos da india",'assunto':"pets",'conteudo':"cuicuicuicuicui"},
+{'id':1,'titulo':"cobrinha",'assunto':"pets",'conteudo':"ssssssssssssss"},
+{'id':2,'titulo':"macaco",'assunto':"pets",'conteudo':"uh uh uh uh"},
+{'id':3,'titulo':'art nouvelle','assunto':'arte','conteudo':'arte dos elfos'},
+{'id':4,'titulo':'arte barroca','assunto':'arte','conteudo':'materia da escola'},
+{'id':5,'titulo':'lançamento elden ring','assunto':'geek','conteudo':'é um jogo que lançou'},
+{'id':6,'titulo':'sonic é bom','assunto':'geek','conteudo':'gotta go fast'},
+]
+assuntos = ['pets','arte']
 
 def getNews():
     return noticias
@@ -10,10 +19,27 @@ def getNews():
 def getNewsPost(id):
     return noticias[id]
 
+def getSubjects():
+    return assuntos
+
+def getSubjectFromId(id):
+    return assuntos[id]
+
+def getNewsSubjects(subject):
+    noticias=[]
+    for news in getNews():
+        if news['assunto'] == subject:
+            noticias.append(news)
+    return noticias
+
 #retorno dos templates
 def home():
     noticias=getNews()
     return render_template("home.html",newsList=noticias)
+
+def homeSubject(subject):
+    noticias = getNewsSubjects(subject)
+    return render_template("homeSubject.html",newsList=noticias,subject=subject)
 
 def newsPost(id):
     noticia = getNewsPost(id)
