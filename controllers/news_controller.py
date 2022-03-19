a82b187027b4e1bf8779695ac33238c0bf8cc6f9
Carolina M <carolinamargiott2i@gmail.com>
2022-03-19 13:14:34 -0300

    adicionado resumo e fazendo ele ser mostrado no home, adicionado cor para titulo e assunto de post de noticia

--- controllers/news_controller.py
+++ controllers/news_controller.py
@@ -3,13 +3,13 @@ from flask import request, jsonify, render_template
 #import dos models
 
 noticias = [
-{'id':0,'titulo':"porquinhos da india",'assunto':"pets",'conteudo':"cuicuicuicuicui"},
-{'id':1,'titulo':"cobrinha",'assunto':"pets",'conteudo':"ssssssssssssss"},
-{'id':2,'titulo':"macaco",'assunto':"pets",'conteudo':"uh uh uh uh"},
-{'id':3,'titulo':'art nouvelle','assunto':'arte','conteudo':'arte dos elfos'},
-{'id':4,'titulo':'arte barroca','assunto':'arte','conteudo':'materia da escola'},
-{'id':5,'titulo':'lançamento elden ring','assunto':'geek','conteudo':'é um jogo que lançou'},
-{'id':6,'titulo':'sonic é bom','assunto':'geek','conteudo':'gotta go fast'},
+{'id':0,'titulo':"porquinhos da india",'assunto':"pets","resumo":"cuicui",'conteudo':"cuicuicuicuicui"},
+{'id':1,'titulo':"cobrinha",'assunto':"pets","resumo":"cobrinha",'conteudo':"ssssssssssssss"},
+{'id':2,'titulo':"macaco",'assunto':"pets","resumo":"como macaco gosta de banana",'conteudo':"uh uh uh uh"},
+{'id':3,'titulo':'art nouvelle','assunto':'arte',"resumo":"arte nova",'conteudo':'arte dos elfos'},
+{'id':4,'titulo':'arte barroca','assunto':'arte',"resumo":"barroco demais",'conteudo':'materia da escola'},
+{'id':5,'titulo':'lançamento elden ring','assunto':'geek',"resumo":"parece dark souls",'conteudo':'é um jogo que lançou'},
+{'id':6,'titulo':'sonic é bom','assunto':'geek',"resumo":"at the speed of sound",'conteudo':'gotta go fast'},
 ]
 assuntos = ['pets','arte']
 
