403661718f6cb383bfb7e801189bea50eabd85db
CarolinaMargiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2022-05-17 10:49:04 -0300

    adicionado host e port para variaveis de ambiente

--- models/envVariables.py
+++ models/envVariables.py
@@ -5,3 +5,5 @@ load_dotenv()
 
 serviceUrl = os.getenv('serviceUrl')
 projectUrl = os.getenv('projectUrl')
+host = os.getenv('host')
+port = os.getenv('port')
--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 from controllers import news_bp,subjects_bp
 from waitress import serve
+from models.envVariables import host,port
 
 app = Flask(__name__)
 app.register_blueprint(news_bp)
 app.register_blueprint(subjects_bp)
 
 if __name__ == '__main__':
-    # app.run()
-    serve(app, host='0.0.0.0', port=5000)
+    # app.run(host=host,port=port)
+    serve(app, host=host, port=port)
 
